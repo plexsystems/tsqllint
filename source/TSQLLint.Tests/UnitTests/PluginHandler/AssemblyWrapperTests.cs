e629e7f2b6e27c0658045d6f31cb1a33c7e4856d
Andy Hall <ahall@plex.com>
2021-04-20 20:07:25 -0400

    Use Assembly.Location like it told me to

--- source/TSQLLint.Tests/UnitTests/PluginHandler/AssemblyWrapperTests.cs
+++ source/TSQLLint.Tests/UnitTests/PluginHandler/AssemblyWrapperTests.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Reflection;
-using Microsoft.VisualStudio.TestPlatform.PlatformAbstractions;
 using NUnit.Framework;
 using TSQLLint.Infrastructure.Plugins;
 
@@ -12,7 +11,7 @@ namespace TSQLLint.Tests.UnitTests.PluginHandler
         {
             get
             {
-                var codeBase = Assembly.GetExecutingAssembly().GetAssemblyLocation();
+                var codeBase = Assembly.GetExecutingAssembly().Location;
                 var uri = new UriBuilder(codeBase);
                 return Uri.UnescapeDataString(uri.Path);
             }
